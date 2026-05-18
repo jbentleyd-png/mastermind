240df91d1c70cfad409770886fdd1d53efcdcedd
Joshua Bentley <232677218+jbentleyd-png@users.noreply.github.com>
2026-05-18 10:34:05 -0600

    Added input confirmation for mode 2

--- lib/player_set_mode.rb
+++ lib/player_set_mode.rb
@@ -9,17 +9,40 @@ module PlayerSetMode
 
   def self.force_input
     input = gets.chomp.upcase
-    until input_allowed?(input, ACCEPTABLE)
+    until input_allowed?(input)
       puts "Please input 4 of the following: #{ACCEPTABLE}"
-      print 'Guess: '
+      print 'Secret Code: '.green
       input = gets.chomp.upcase
     end
     input
+  end
+
+  def self.y_n?(confirm_input) = %w[Y N].include?(confirm_input)
+
+  def self.force_y_n
+    input = gets.chomp.upcase
+    until y_n?(input)
+      print 'Confirm (Y/N): '
+      input = gets.chomp.upcase
+    end
+    input
+  end
+
+  def self.input_and_confirm?
+    puts "Secret code is #{force_input}. Is this correct?"
+    print 'Confirm (Y/N): '
+    confirmation = force_y_n
+    confirmation == 'Y'
   end
 
   def self.play_game
     puts "\n\nLet\'s play Mastermind! Create a four-color code (color repeats are possible).".blue
     puts "Possible inputs: #{ACCEPTABLE}"
     print "\nSecret Code: ".green
+    if input_and_confirm?
+      print "Let's play, then."
+    else
+      print 'Reinput.'
+    end
   end
 end
