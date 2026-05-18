b4cc7fff19008eb9dbe89e5c923a3a975f124d32
Joshua Bentley <232677218+jbentleyd-png@users.noreply.github.com>
2026-05-18 09:10:54 -0600

    Moved original game mode to module.
    Plays just fine.

---
+++ lib/player_guess_mode.rb
@@ -0,0 +1,48 @@
+module PlayerGuessMode
+  def self.input_allowed(input, allowed)
+    return false if input.length != 4
+
+    input = input.chars
+    difference_array = input - allowed
+    difference_array.empty?
+  end
+
+  def self.force_input
+    input = gets.chomp.upcase
+    until input_allowed(input, ACCEPTABLE)
+      puts "Please input 4 of the following: #{ACCEPTABLE}"
+      print 'Guess: '
+      input = gets.chomp.upcase
+    end
+    input
+  end
+
+  def self.guess_input(round_number)
+    prompt = round_number < 10 ? "##{round_number}: " : "##{round_number}:"
+    print prompt
+    force_input.chars
+  end
+
+  def self.play_round(board, round_number)
+    guess_array = guess_input(round_number)
+    board.add_row(guess_array)
+    board.display
+  end
+
+  def self.play_game
+    code = Code.new
+    board = Board.new(code)
+    board.display
+    puts 'Let\'s play Mastermind! Try to guess the four-color code (color repeats are possible).'
+    (1..12).each do |round_number|
+      play_round(board, round_number)
+      break if code.solved == true
+    end
+    if code.solved == true
+      puts 'YOU DID IT! YOU ARE THE CHOSEN ONE!'.blue
+    else
+      code_output = code.pegs.join('')
+      puts "You FAIL. Code was #{code_output}.".red
+    end
+  end
+end
--- main.rb
+++ main.rb
@@ -4,54 +4,8 @@ require_relative 'lib/code'
 require_relative 'lib/guess'
 require_relative 'lib/board_row'
 require_relative 'lib/board'
+require_relative 'lib/player_guess_mode'
 
 ACCEPTABLE = %w[R G B W P Y].freeze
 
-def input_allowed(input, allowed)
-  return false if input.length != 4
-
-  input = input.chars
-  difference_array = input - allowed
-  difference_array.empty?
-end
-
-def force_input
-  input = gets.chomp.upcase
-  until input_allowed(input, ACCEPTABLE)
-    puts "Please input 4 of the following: #{ACCEPTABLE}"
-    print 'Guess: '
-    input = gets.chomp.upcase
-  end
-  input
-end
-
-def guess_input(round_number)
-  prompt = round_number < 10 ? "##{round_number}: " : "##{round_number}:"
-  print prompt
-  force_input.chars
-end
-
-def play_round(board, round_number)
-  guess_array = guess_input(round_number)
-  board.add_row(guess_array)
-  board.display
-end
-
-def play_mastermind
-  code = Code.new
-  board = Board.new(code)
-  board.display
-  puts 'Let\'s play Mastermind! Try to guess the four-color code (color repeats are possible).'
-  (1..12).each do |round_number|
-    play_round(board, round_number)
-    break if code.solved == true
-  end
-  if code.solved == true
-    puts 'YOU DID IT! YOU ARE THE CHOSEN ONE!'.blue
-  else
-    code_output = code.pegs.join('')
-    puts "You FAIL. Code was #{code_output}.".red
-  end
-end
-
-play_mastermind
+PlayerChoiceMode.play_game
